8ec382fbb867aa90290cf6e54f4691f24758d1df
minhluan306288 <minhluan306288@gmail.com>
2022-11-30 15:29:51 +0700

    change index

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/*', function(req, res){
-    res.sendFile(path.resolve(__dirname,'public', 'index.html'));
+    res.sendFile(path.resolve('index.html'));
 })
 
 app.listen(process.env.PORT || 8080, () => {
