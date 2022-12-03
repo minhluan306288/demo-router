8ad43f3441c66d8993df7455e2917c5728c4ee22
minhluan306 <minhluan306288@gmail.com>
2022-12-03 15:00:45 +0700

    dist

---
+++ assets/main.js
@@ -0,0 +1,36 @@
+const historyPath = '/demo-router';
+let routes = [
+    {
+        path: '/',
+        component: pageHome,
+    },
+    {
+        path: '/about',
+        component: pageAbout,
+    },
+    {
+        path: '/*',
+        component: pageNotFound,
+    },
+];
+let appView = document.getElementById('root');
+function pageHome() {
+    appView.innerHTML = `<div> Home </div>`;
+}
+function pageAbout() {
+    appView.innerHTML = `<div> About </div>`;
+}
+function pageNotFound() {
+    appView.innerHTML = `<div> Not Found </div>`;
+}
+function router(_routes) {
+    let currentPath = window.location.pathname;
+    currentPath = currentPath.replace(historyPath, '');
+    const currentRoute = _routes.filter(i => currentPath === i.path);
+    console.log('newCurrent:', currentRoute);
+    if (currentRoute[0])
+        return currentRoute[0].component();
+    else
+        return _routes.filter(i => '/*' === i.path)[0].component();
+}
+window.addEventListener('load', router(routes));
